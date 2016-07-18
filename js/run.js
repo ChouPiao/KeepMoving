a8c928438ce70ce3a41c173f169b522acb5fd7dc
ChouPiao <zbzjut@aliyun.com>
2016-07-18 10:50:41 +0800

    Add files via upload
    Don't ask.Just keep moving.

---
+++ js/run.js
@@ -0,0 +1,50 @@
+function colorBarAnimframe()  {
+  
+  this.div,
+  this.progress,
+  this.speed,
+  
+  this.init = function(index) {
+    this.div = document.getElementById("color_bar" + index);
+    this.progress = Math.floor(Math.random() * 100) + 0.1;
+    this.newSpeed();
+    this.newBackground();
+  }
+
+  this.nextFrame = function() {
+    this.progress += this.speed;
+    if (this.progress > 100) {
+      this.progress = 0;
+      this.newSpeed();
+      this.newBackground();
+    }
+    this.div.style.width = this.progress + "%";
+  }
+
+  this.newSpeed = function()  {
+    this.speed = Math.random();
+  }
+
+  this.newBackground = function() {
+    this.div.style.background = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+  }
+}
+var barList = new Array();
+var colorBar;
+for (var i = 0; i < 10; ++ i) {
+  colorBar = new colorBarAnimframe();
+  colorBar.init(i);
+  barList[i] = colorBar;
+}
+
+var progress = 0;
+
+function step(timestamp) {
+  for (var i = 0; i < 10; ++ i) {
+    barList[i].nextFrame();
+  }
+
+  requestAnimationFrame(step);
+}
+
+requestAnimationFrame(step);
